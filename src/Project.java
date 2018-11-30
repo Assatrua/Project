41cdf2994d1e4c2d8b7ca86f2b0ba595a6bba056
DHaidenko <dgaidenko@gmail.com>
2018-11-30 21:11:01 +0200

    asdfghjkl;'

--- src/Project.java
+++ src/Project.java
@@ -3,6 +3,6 @@
  */
 public class Project {
     public static void main(String[] args) {
-
+        System.out.println();
     }
 }
---
+++ src/test/B.java
@@ -0,0 +1,7 @@
+package test;
+
+/**
+ * Created by Student on 30.11.2018.
+ */
+public class B {
+}
